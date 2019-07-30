e01403d1b814b294e52dce1347a202d816a92e35
Claudia Seldner <claudiaseldner@Claudias-MacBook-Pro-2.local>
2019-07-29 20:23:19 -0500

    Adding files

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,87 +1,54 @@
-//load  npm's
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-// create a db connection
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "@Rl7trou3",
-    database: "bamazon_db"
+  host: "localhost",
+  // Port; if not 3306
+  port: 3306,
+  // username
+  user: "root",
+  // Password
+  password: process.env.PASSWORD,
+  database: "bamazon"
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
-    queryProducts();
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+    productsAvailable();
+    buy();
 });
 
-function queryProducts() {
+function productsAvailable() {
+        console.log("Selecting all products...\n");
+        connection.query("SELECT * FROM products", function(err, res) {
+          if (err) throw err;
+          // Log all results of the SELECT statement
+          console.log(res);
+          connection.end();
+        });
+      }
 
-    connection.query("select * from products", function (err, res) {
-        if (err) throw err;
-        //transform to set the item_id value to the index colum of the console.table 
-        const transformed = res.reduce((acc, { item_id, ...x }) => { acc[item_id] = x; return acc }, {});
-        console.table(transformed)
-        doWhat();
-    });
-}
-function buyProduct(item, units) {
-
-    connection.query("select * from products where item_id = ?", item, function (err, res) {
-        if (err) throw err;
-        //transform to set the item_id value to the index colum of the console.table 
-        const transformed = res.reduce((acc, { item_id, ...x }) => { acc[item_id] = x; return acc }, {});
-        console.table(transformed)
-        var stock = parseInt(res[0].stock_quantity);
-        if (stock > 0 && (stock - units) >= 0) {
-            stock = stock - units;
-            var sale = (res[0].price*units);
-            console.log("Your total will be: "+sale);
-            
-            updateProduct(stock, item,sale);
-        } else {
-            console.log("Insufficient quantity!")
-        }
-    });
-}
-function updateProduct(stock, product,sale) {
-    connection.query("update products set ? where ?", [{ stock_quantity: stock, product_sales: sale}, { item_id: product }], function (err, res) {
-        if (err) throw err;
-        queryProducts();
-    });
-}
-function doWhat() {
+      // function which prompts the user for what action they should take
+function buy() {
     inquirer
-        .prompt([
-            {
-                type: "list",
-                message: "What you like to do:",
-                choices: ["Buy", "Exit"],
-                name: "option"
-            }
-        ])
-        .then(function (input) {
-            if (input.option === "Buy") {
-                inquirer
-                    .prompt([
-                        {
-                            type: "input",
-                            message: "What porduct you what to buy?",
-                            name: "product"
-                        },
-                        {
-                            type: "input",
-                            message: "How many units you what to buy?",
-                            name: "units"
-                        }
-                    ]).then(function(input){
-                        buyProduct(input.product, input.units);
-                    })
-                
-            } else {
-                connection.end();
-                return
-            }
-        })
-}
+      .prompt({
+        id: "ID to buy",
+        type: "list",
+        message: "What is the ID that you would like to buy?",
+        choices: ["BUY"]
+      })
+      .then(function(answer) {
+        // based on their answer, either call the bid or the post functions
+        if (answer.buy=== "POST") {
+          buy();
+        }
+        else if(answer.postOrBid === "BID") {
+          bidAuction();
+        } else{
+          connection.end();
+        }
+      });
+  }
+  
+        
--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -10,11 +10,6 @@ CREATE TABLE products (
   stock_quantity int(10) NOT NULL,
   PRIMARY KEY (id)
   );
-  DROP TABLE songs;
   
-  
-
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ('Samsung S10+', 'Cellphones', '18999', '20'), ('Xiaomi Redmi Note 7 128GB', 'Cellphones', '4699', '100'), ('Apple Airpods Wireless', 'Audio', '3699', '50'), ('Apple iPhone XR 64GB Red', 'Cellphones', '16999', '25'), ('Huawei P30 Lite 128GB', 'Cellphones', '6499', '35'), ('Apple iPad Mini 4', 'Computing', '10699', '25'), ('Bose Soundlink Revolve+', 'Audio', '6499', '80'), ('Apple iPhone XS 64GB', 'Cellphones', '19999', '30'), ('Samsung A30', 'Cellphones', '4299', '30'), ('Samsung Galaxy Buds', 'Audio', '2799', '80');
-
-SELECT * FROM products;
